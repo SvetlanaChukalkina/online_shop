04f32048e6d270636acd5c5812b3b6d04a1a2838
Svetlana Chukalkina <chukalkinasa@yandex.ru>
2025-08-24 22:25:31 +0300

    small fixes

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,8 +1,9 @@
 from django.core.exceptions import ValidationError
 from django.forms import ModelForm, BooleanField
 
-from catalog.constants import forbidden_list
 from catalog.models import Product
+from config.settings import FORBIDDEN_LIST
+
 
 class StyleFormMixin:
     def __init__(self, *args, **kwargs):
@@ -26,14 +27,14 @@ class ProductForm(StyleFormMixin, ModelForm):
 
     def clean_name(self):
         name = self.cleaned_data.get('name', '')
-        for word in forbidden_list:
+        for word in FORBIDDEN_LIST:
             if word in name.lower():
                 raise ValidationError(f'Имя продукта не может содержать слово {word}')
         return name
 
     def clean_description(self):
         description = self.cleaned_data.get('description', '')
-        for word in forbidden_list:
+        for word in FORBIDDEN_LIST:
             if word in description.lower():
                 raise ValidationError(f'Описание продукта не может содержать слово {word}')
         return description
